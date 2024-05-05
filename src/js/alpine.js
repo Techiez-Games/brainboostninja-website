94530ee3d7815fcec2e3abb7ad374e6597404ba0
MTulaimat <mamjt@hotmail.com>
2024-05-05 04:17:33 +0400

    more work and stuff

--- src/js/alpine.js
+++ src/js/alpine.js
@@ -1,16 +1,51 @@
 window.alpine = {
   menu: [
     {
-      label: "Home",
+      label: "Typing Ninja",
       url: "/home",
     },
     {
-      label: "About",
+      label: "Feedback",
       url: "/about",
     },
     {
-      label: "Game",
+      label: "PowerUps",
+      url: "/ninjaboosting",
+    },
+    {
+      label: "Subscribe",
       url: "/ninjaboosting",
     },
   ],
-};
+
+  powerupCards: [
+    {
+      name: "Fast building",
+      description:
+        "Tailor Anima's Landing Page UI Kit to your unique style and brand with customisable components, in no time!",
+      icon: "/assets/images/powerups/ice.svg",
+      video: "/assets/videos/test video.mp4",
+    },
+    {
+      name: "Fast building",
+      description:
+        "Tailor Anima's Landing Page UI Kit to your unique style and brand with customisable components, in no time!",
+      icon: "/assets/images/powerups/ice.svg",
+      video: "/assets/videos/test video.mp4",
+    },
+    {
+      name: "Fast building",
+      description:
+        "Tailor Anima's Landing Page UI Kit to your unique style and brand with customisable components, in no time!",
+      icon: "/assets/images/powerups/ice.svg",
+      video: "/assets/videos/test video.mp4",
+    },
+  ],
+
+  socials: [
+    {
+      icon: "fa-brands fa-steam",
+      url: "/asdasd",
+    },
+  ],
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,42 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      padding: {
+        main: "3.125rem",
+      },
+      colors: {
+        brand: {
+          zircon: "#F8F9FF",
+          alto: "#D8D8D8",
+          corn: "#F8D57E",
+          persimmon: "#FF6250",
+          pink: "#FF5C7C",
+          lightpink: "#FFEBEF",
+          gold: "#E8B485",
+          dark1: "#324253",
+          dark2: "#232C38",
+          dark3: "#010101",
+          outline: "#FFDCBC",
+        },
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        mulish: ["Mulish", "sans-serif"],
+      },
+      // fontSize: {
+      //   xs: [".75rem", "1rem"], // 12px font size with 16px line height
+      //   sm: [".875rem", "1.25rem"], // 14px font size with 20px line height
+      //   base: ["1rem", "1.5rem"], // 16px font size with 24px line height
+      //   lg: ["1.125rem", "1.75rem"], // 18px font size with 28px line height
+      //   xl: ["1.25rem", "1.75rem"], // 20px font size with 28px line height
+      //   "2xl": ["1.5rem", "2rem"], // 24px font size with 32px line height
+      // },
+    },
   },
   plugins: [
     "prettier-plugin-tailwindcss",
     require("@tailwindcss/typography"),
     require("@tailwindcss/forms"),
   ],
-};
+}
